1495e00b9d0e491f264ac3ce812a1f4508d1ca15
inkspotting <fromphototoa4@gmail.com>
2023-05-18 17:05:53 +0200

    Include storybook for testing & documentation purposes

--- src/components/progressPie/ProgressPie.tsx
+++ src/components/progressPie/ProgressPie.tsx
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './progressPie.scss';
 
+const calcPercentage = (partialValue: number, totalValue: number) =>
+  (100 * partialValue) / totalValue;
+
 interface ProgressPieProps {
-  hundredPercentEquiv: number;
-  currentProgressValue: number;
+  /**
+   * a number that represents a 100% value (100 by default)?
+   */
+  hundredPercentEquiv: number | null | undefined;
+  /**
+   * a number that represents current progress (0 by default)
+   */
+  currentProgressValue: number | null | undefined;
 }
 
+/**
+ * Progress-pie component for creating progress bars in circular fashion.
+ */
 export const ProgressPie = (props: ProgressPieProps) => {
-  const { hundredPercentEquiv } = props;
+  const { hundredPercentEquiv = 100, currentProgressValue } = props;
+
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    // eslint-disable-next-line no-debugger
+    debugger;
+    if (!currentProgressValue) {
+      setProgress(0);
+      return;
+    }
+    setProgress(calcPercentage(hundredPercentEquiv || 100, currentProgressValue));
+  }, [currentProgressValue, hundredPercentEquiv]);
+
   return (
     <div className="container w-[300px] h-[300px]">
       <div className="c100 p10 blue">
@@ -17,17 +42,6 @@ export const ProgressPie = (props: ProgressPieProps) => {
           <div className="fill"></div>
         </div>
       </div>
-      {/*<div className={styles.circleWrap}>*/}
-      {/*  <div className="circle">*/}
-      {/*    <div className={`${styles.mask} ${styles.full}`}>*/}
-      {/*      <div className={styles.fill}></div>*/}
-      {/*    </div>*/}
-      {/*    <div className={`${styles.mask} ${styles.half}`}>*/}
-      {/*      <div className={styles.fill}></div>*/}
-      {/*    </div>*/}
-      {/*    <div className={styles.insideCircle}>70%</div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </div>
   );
 };
---
+++ src/stories/ProgressPie.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ProgressPie } from '../components/progressPie/ProgressPie';
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof ProgressPie> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'ProgressPie',
+  component: ProgressPie,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof ProgressPie>;
+
+export const FirstStory: Story = {
+  args: {
+    currentProgressValue: 15,
+    hundredPercentEquiv: 100,
+    //👇 The args you need here will depend on your component
+  },
+};
