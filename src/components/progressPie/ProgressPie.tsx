59a417a1fd4158c76d2dd5f89bd53d31d97e325f
inkspotting <fromphototoa4@gmail.com>
2023-05-26 18:37:41 +0200

    Implement second circle that'll cover over 100 percentage case

--- src/components/progressPie/ProgressPie.tsx
+++ src/components/progressPie/ProgressPie.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+
 import './progressPie.scss';
 
 const calcPercentage = (partialValue: number, totalValue: number) =>
@@ -7,12 +8,14 @@ const calcPercentage = (partialValue: number, totalValue: number) =>
 export interface ProgressPieConfig {
   barColorBackground: string;
   progressBarBorderColor: string;
+  progressBarOverflowBorderColor: string;
   width?: number;
 }
 
 export const defaultProgressPieConfig: ProgressPieConfig = {
   barColorBackground: 'bg-gray-50',
   progressBarBorderColor: 'border-green-400',
+  progressBarOverflowBorderColor: 'border-green-600',
 };
 
 interface ProgressPieProps {
@@ -40,6 +43,8 @@ function ProgressPie(props: ProgressPieProps) {
   const { hundredPercentEquiv = 100, currentProgressValue, config } = props;
 
   const [progress, setProgress] = useState(0);
+  const [progressOverflow, setProgressOverflow] = useState(0);
+  const [totalProgress, setTotalProgress] = useState(0);
   const [pieChartConfig, setPieChartConfig] = useState<ProgressPieConfig>(defaultProgressPieConfig);
 
   useEffect(() => {
@@ -47,7 +52,14 @@ function ProgressPie(props: ProgressPieProps) {
       setProgress(0);
       return;
     }
-    setProgress(calcPercentage(currentProgressValue, hundredPercentEquiv || 100));
+    const progressPercentage = calcPercentage(currentProgressValue, hundredPercentEquiv || 100);
+    setTotalProgress(progressPercentage);
+    setProgress(progressPercentage <= 100 ? progressPercentage : 100);
+    let percentageOverflow = 0;
+    if (progressPercentage > 100) {
+      percentageOverflow = progressPercentage - 100 < 200 ? progressPercentage - 100 : 200;
+    }
+    setProgressOverflow(percentageOverflow);
   }, [currentProgressValue, hundredPercentEquiv]);
 
   useEffect(() => {
@@ -59,11 +71,17 @@ function ProgressPie(props: ProgressPieProps) {
 
   return (
     <div style={{ width: config?.width || 300, height: config?.width || 300 }}>
-      <div className={`c100 p${progress} ${config?.barColorBackground} w-full h-full`}>
-        <span>{progress}%</span>
+      <div
+        className={`c100 p${progress} o${progressOverflow} ${config?.barColorBackground} w-full h-full`}
+      >
+        <span>{totalProgress}%</span>
         <div className="slice">
-          <div className={`bar ${pieChartConfig.progressBarBorderColor} border-amber-400`} />
-          <div className={`fill ${pieChartConfig.progressBarBorderColor} border-amber-400`} />
+          <div className={`bar ${pieChartConfig.progressBarBorderColor}`} />
+          <div className={`fill ${pieChartConfig.progressBarBorderColor}`} />
+        </div>
+        <div className="slice-overflow">
+          <div className={`bar-overflow ${pieChartConfig.progressBarOverflowBorderColor}`} />
+          <div className={`fill-overflow ${pieChartConfig.progressBarOverflowBorderColor}`} />
         </div>
       </div>
     </div>
